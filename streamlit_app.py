7dde9c905f1c9430a141063ccc13e70ede7e5cf7
gsillSnowflake <117941721+gsillSnowflake@users.noreply.github.com>
2022-11-10 12:20:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.text('🥑 🥪 Avocado Toast')
 streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
 
 
-my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
