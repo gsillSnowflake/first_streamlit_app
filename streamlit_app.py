9fbfb3b118bb73a2f751cd3623e8f7a0b1febf5e
gsillSnowflake <117941721+gsillSnowflake@users.noreply.github.com>
2022-11-11 16:12:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,7 @@ import pandas
 import snowflake.connector
 import requests
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+
 
 
 streamlit.title('My Parents New Healthy Diner')
@@ -43,3 +38,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put in table
 streamlit.dataframe(fruityvice_normalized)
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
