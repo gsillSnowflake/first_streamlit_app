ee56a99f1de28e09cb38e50663bbf0b5628ce33b
gsillSnowflake <117941721+gsillSnowflake@users.noreply.github.com>
2022-11-11 16:30:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,22 +23,25 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+
+def get_fruityvice_data(this_fruit_cjoice);
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-      # loads data into variable
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      back_from_function = get_fruityvice_data(fruit_choice)
+      
       # put in table
-      streamlit.dataframe(fruityvice_normalized)
+      streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
